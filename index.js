a2fed176886b241895812db1ca955f1b9e869b91
Joe Fiorini <joe@joefiorini.com>
2015-04-28 19:30:54 -0400

    Support class names instead of IDs
    Allow for writing class names instead of IDs on symbol tags. Also allows specifying a SUITCSS component name for adhering to BEM standards.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require("fs"),
-	path = require("path"),
-	mkdirp = require("mkdirp"),
-	Writer = require("broccoli-writer"),
-	helpers = require("broccoli-kitchen-sink-helpers"),
-	cheerio = require("cheerio");
+  path = require("path"),
+  mkdirp = require("mkdirp"),
+  Writer = require("broccoli-writer"),
+  helpers = require("broccoli-kitchen-sink-helpers"),
+  cheerio = require("cheerio");
 
 module.exports = SvgProcessor;
 SvgProcessor.prototype = Object.create(Writer.prototype);
@@ -11,68 +11,90 @@ SvgProcessor.prototype.constructor = SvgProcessor;
 
 function SvgProcessor (inputTree, options) {
 
-	if (!(this instanceof SvgProcessor)) return new SvgProcessor(inputTree, options);
+  if (!(this instanceof SvgProcessor)) return new SvgProcessor(inputTree, options);
 
-	this.inputTree = inputTree;
+  this.inputTree = inputTree;
 
-	this.options = {
-		outputFile: "/images.svg"
-	};
+  this.options = {
+    outputFile: "/images.svg",
+    preferClasses: false
+  };
 
-	for (key in options) {
-		if (options.hasOwnProperty(key)) {
-			this.options[key] = options[key];
-		}
-	}
+  for (key in options) {
+    if (options.hasOwnProperty(key)) {
+      this.options[key] = options[key];
+    }
+  }
 
 };
 
 SvgProcessor.prototype.write = function (readTree, destDir) {
 
-	var self = this;
+  var self = this;
 
-	return readTree(this.inputTree).then(function (srcDir) {
+  return readTree(this.inputTree).then(function (srcDir) {
 
-		var output = ["<svg xmlns='http://www.w3.org/2000/svg' style='display: none'>"];
+    var output = ["<svg xmlns='http://www.w3.org/2000/svg' style='display: none'>"];
 
-		try {
+    try {
 
-			var inputFiles = helpers.multiGlob(["**/*.svg"], { cwd: srcDir });
-			for (var i = 0; i < inputFiles.length; i++) {
-				var stat = fs.statSync(srcDir + "/" + inputFiles[i]);
-				if (stat && stat.isFile()) {
-					var fileContents = fs.readFileSync(srcDir + "/" + inputFiles[i], { encoding: "utf8" });
-					output.push(parseSvg(inputFiles[i], fileContents));
-				}
-			}
+      var inputFiles = helpers.multiGlob(["**/*.svg"], { cwd: srcDir });
+      for (var i = 0; i < inputFiles.length; i++) {
+        var stat = fs.statSync(srcDir + "/" + inputFiles[i]);
+        if (stat && stat.isFile()) {
+          var fileContents = fs.readFileSync(srcDir + "/" + inputFiles[i], { encoding: "utf8" });
+          output.push(parseSvg(inputFiles[i], fileContents, self.options));
+        }
+      }
 
-		} catch (error) {
-			if (!error.message.match("did not match any files")) {
-				throw error;
-			}
-		}
+    } catch (error) {
+      if (!error.message.match("did not match any files")) {
+        throw error;
+      }
+    }
 
-		output.push("</svg>");
+    output.push("</svg>");
 
-		helpers.assertAbsolutePaths([self.options.outputFile]);
-		mkdirp.sync(path.join(destDir, path.dirname(self.options.outputFile)));
-		var concatenatedOutput = output.join("\n");
-		fs.writeFileSync(path.join(destDir, self.options.outputFile), concatenatedOutput);
+    helpers.assertAbsolutePaths([self.options.outputFile]);
+    mkdirp.sync(path.join(destDir, path.dirname(self.options.outputFile)));
+    var concatenatedOutput = output.join("\n");
+    fs.writeFileSync(path.join(destDir, self.options.outputFile), concatenatedOutput);
 
-	});
+  });
 
 };
 
-function parseSvg (filename, fileContents) {
+function camelize(str) {
+    var first = str[0];
+    var rest = str.slice(1, str.length);
 
-	var $fileContents = cheerio.load(fileContents, { xmlMode: true }),
-		$svg = $fileContents("svg"),
-		viewBox = $svg.attr("viewBox"),
-		$outputContents = cheerio.load("<symbol id='" + path.basename(filename).replace(/\.[^/.]+$/, "") + "' viewBox='" + viewBox + "'></symbol>", { xmlMode: true }),
-		$symbol = $outputContents("symbol");
+    return first.toLowerCase() + rest;
+}
 
-	$symbol.html($svg.html());
+function parseSvg (filename, fileContents, options) {
 
-	return $outputContents.html();
+  var $fileContents = cheerio.load(fileContents, { xmlMode: true }),
+    $svg = $fileContents("svg"),
+    viewBox = $svg.attr("viewBox");
+    var tag = "<symbol ";
+    var identifer = path.basename(filename).replace(/\.[^/.]+$/, "");
+
+    if (options.preferClasses) {
+        if (options.suitComponent) {
+            identifer = options.suitComponent + " " + options.suitComponent + "--" + camelize(identifer);
+        }
+        tag += "class='" + identifer + "'";
+    } else {
+        tag += "id='" + identifer + "'";
+    }
+
+    tag += " viewBox='" + viewBox + "'></symbol>";
+
+    $outputContents = cheerio.load(tag, { xmlMode: true }),
+    $symbol = $outputContents("symbol");
+
+  $symbol.html($svg.html());
+
+  return $outputContents.html();
 
 };
